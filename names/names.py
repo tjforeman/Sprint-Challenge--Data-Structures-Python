d882f51f2a68dfeb1528eec1c2646eea0abde1e1
tjforeman <tylerforeman92@gmail.com>
2019-11-15 13:59:31 -0500

    mvp met

--- names/names.py
+++ names/names.py
@@ -1,4 +1,6 @@
 import time
+from binary_search_tree import BinarySearchTree 
+from doubly_linked_list import DoublyLinkedList
 
 start_time = time.time()
 
@@ -10,13 +12,31 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+duplicates = DoublyLinkedList()
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+tree = BinarySearchTree(' ')
+
+for names in names_1:
+    tree.insert(names)
+
+for names in names_2:
+    if tree.contains(names):
+        duplicates.add_to_tail(names)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"{duplicates.length} duplicates:\n\n\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+# tree = BinarySearchTree(' ')
+
+# for names in name_1:
+#     tree.insert(names)
+
+# for names in names_2:
+#     if tree.contains(names):
+#         duplicates.append(names)
 
